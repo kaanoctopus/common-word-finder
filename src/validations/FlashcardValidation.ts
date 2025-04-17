6638d90466745f512527e067b26de46abe3548a7
kaanoctopus <kaan@octopussignage.com>
2025-04-17 09:03:43 +0300

    token bug fix

--- src/validations/FlashcardValidation.ts
+++ src/validations/FlashcardValidation.ts
@@ -1,20 +1,17 @@
 import { body } from "express-validator";
 
 export const addWordFlashcardValidator = [
-    body("token").isString().withMessage("Token is required"),
     body("word").isString().withMessage("Word is required"),
     body("meanings").isArray().withMessage("Meanings are required"),
 ];
 
 export const addWordsBulkValidator = [
-    body("token").isString().withMessage("Token is required"),
     body("words").isArray().withMessage("Words are required"),
     body("meanings").isArray().withMessage("Meanings are required"),
     body("meanings.*").isString().withMessage("Each meaning must be a string"),
 ];
 
 export const reviewFlashcardValidator = [
-    body("token").isString().withMessage("Token is required"),
     body("word").isString().withMessage("Word is required"),
     body("answer").isBoolean().withMessage("Answer is required"),
 ];
