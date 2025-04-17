4bdd46fbcfcec1a98495baec72a98b5b52d0ed74
kaanoctopus <kaan@octopussignage.com>
2025-04-17 12:53:00 +0300

    netlify test

---
+++ src/netlify/functions/backend.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import serverless from "serverless-http";
+import { PrismaClient } from "@prisma/client";
+
+import parserRoutes from "../../routes/ParserRoutes";
+import dictonaryRoutes from "../../routes/DictionaryRoutes";
+import authenticationRoutes from "../../routes/AuthenticationRoutes";
+import flashcardRoutes from "../../routes/FlashcardRoutes";
+import { rateLimiter } from "../../middlewares/RateLimiter";
+
+const app = express();
+
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(rateLimiter);
+app.use(bodyParser.json({ limit: "50mb" }));
+
+async function testConnection() {
+    try {
+        await prisma.$connect();
+        console.log("Prisma connected to the database");
+    } catch (error) {
+        console.error("Prisma connection error:", error);
+    }
+}
+
+testConnection();
+
+
+app.use("/api/", parserRoutes);
+app.use("/api/", dictonaryRoutes);
+app.use("/api/", authenticationRoutes);
+app.use("/api/", flashcardRoutes);
+
+module.exports.handler = serverless(app);
